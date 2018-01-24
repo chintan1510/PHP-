f5e649ad461e562e2538a881c10f4b12feb6cf1b
root <chintan.panjwani@gmail.com>
2018-01-24 19:04:07 +0530

    7.04 commit

--- sample.php
+++ sample.php
@@ -1,7 +1,22 @@
 <?php
 
-echo "<form action='sample.php' method='POST'>";
+/*echo "<form action='sample.php' method='POST'>";
 echo "<input type=submit value='View User Details' name='submit'>";
-echo "</form>";
+echo "</form>";*/
+
+$names = array("Chintan","Ankush","Parth","Clinton","Brij",1,2);
+
+//sort($names,1);
+$alength = count($names);
+for($i=0;$i<$alength;$i++)
+{
+	echo $names[$i]." ";
+}
+
+list($a,$b) = $names;
+echo $a;
+
+echo array_sum($names);
+
 
 ?>
